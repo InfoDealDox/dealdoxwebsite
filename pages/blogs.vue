813854d7235e722598de8a1b60aa19d444f848b3
AntixxTechHub <info@antixxtechhub.com>
2023-09-26 22:43:20 +0530

    blog image fixed

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
         <Navbar class="navbar-style-two" />
-        <PageTitle class="page-title-style-two" pageTitle=""
-            pageDesc="" />
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12 col-md-12">
+                    <div class="new-app-main-banner-wrap-image">
+                        <img src="../assets/images/more-home/banner/blogsheader.png" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <PageTitle class="page-title-style-two" pageTitle=""
+            pageDesc="" /> -->
         <Blog />
         <LatestBlog />
         <AutomatedQuotes />
@@ -12,7 +21,7 @@
 
 <script>
 import Navbar from '../layouts/Navbar';
-import PageTitle from '../components/Common/PageTitle';
+// import PageTitle from '../components/Common/PageTitle';
 import Blog from '../components/blogs/Blog'
 import LatestBlog from '../components/blogs/LatestBlog'
 import AutomatedQuotes from '../components/Common/AutomatedQuotes';
@@ -21,7 +30,7 @@ import DealDoxFooter from '../layouts/DealDoxFooter'
 export default {
     components: {
         Navbar,
-        PageTitle,
+        // PageTitle,
         Blog,
         LatestBlog,
         AutomatedQuotes,
