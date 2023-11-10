e75c83dc57b2ae3df6a440c5d156cae9138a7aa4
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-11-10 12:26:13 +0530

    # is removed from navbar

--- layouts/Navbar.vue
+++ layouts/Navbar.vue
@@ -3,7 +3,7 @@
         <div class="jexa-nav">
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <NuxtLink class="navbar-brand" to="/#">
+                    <NuxtLink class="navbar-brand" to="/">
                         <img src="https://res.cloudinary.com/ds6j6gokv/image/upload/v1694695208/logo_m8wwsx.svg" alt="logo">
                     </NuxtLink>
 
