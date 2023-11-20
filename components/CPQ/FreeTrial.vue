940093470ec9fb8fbff784e42d44552125ed8460
BhagyashreeKathani-SPM-DealDocx <bhagyashree.k@spmglobaltech.com>
2023-11-20 13:43:52 +0530

    updated-3

--- components/CPQ/FreeTrial.vue
+++ components/CPQ/FreeTrial.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="new-free-trial-area bg-white ptb-100">
-        <div class="container">
+    <div class="container"
+        <div class="maindiv">
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12">
                     <div class="features-item-free-quotes with-border bg-grey">
@@ -8,15 +9,22 @@
                             <img src="../../assets/images/do-you-know-tag.svg" alt="image">
                         </div>
                         <h3>Automated Quote Generation tool helps businesses achieve faster sales cycles and higher win
-                            rates by providing accurate pricing and quotes in a timely manner. Get started free onboarding
+                            rates by providing accurate pricing and quotes in a timely manner. Get started with free onboarding
                             today!</h3>
                         <NuxtLink to="/sign-up-free-trial" class="default-btn">Start My Free Trial</NuxtLink>
                     </div>
                 </div>
             </div>
-        </div>
+              <div class="faqDiv">
+<h3>Automated Quote Generation tool helps businesses achieve faster sales cycles and higher win
+                            rates by providing accurate pricing and quotes in a timely manner. Get started free onboarding
+                            today!</h3>
+      </div>
+    </div>
+  
     </div>
 </template>
+ 
 
 <script>
 export default {
