0f98b9f366458f3eb5c4c31da6a18bc003da6806
AntixxTechHub <info@antixxtechhub.com>
2023-11-27 17:54:24 +0530

    cpm services faq hide

--- components/Common/servicefaq.vue
+++ components/Common/servicefaq.vue
@@ -4,8 +4,10 @@
             <div class="features-content-improve-sales">
                 <div class="section-title">
                     <h1>Frequently asked questions</h1>
-                    <p>Can’t find your query listed? <span class="sub-title-faq"><NuxtLink to="/contact-us">
-                            Write us</NuxtLink></span> your query</p>
+                    <p>Can't find your query listed? <span class="sub-title-faq">
+                            <NuxtLink to="/contact-us">
+                                Write us</NuxtLink>
+                        </span> your query</p>
                 </div>
             </div>
             <div class="row align-items-center">
@@ -16,11 +18,13 @@
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
                                     <button class="accordion-title">
-                                     What is CPQ software, and why is it important for my business? </button>
+                                        What is CPQ software, and why is it important for my business? </button>
                                 </template>
                                 <template v-slot:accordion-content>
                                     <div class="accordion-body">
-                                        <p>CPQ software streamlines sales processes by configuring products, setting pricing, and generating quotes, making it vital for improving sales operations and driving revenue. 
+                                        <p>CPQ software streamlines sales processes by configuring products, setting
+                                            pricing, and generating quotes, making it vital for improving sales operations
+                                            and driving revenue.
                                         </p>
                                     </div>
                                 </template>
@@ -28,25 +32,28 @@
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
                                     <button class="accordion-title">
-                                      How can I choose the best CPQ software for my business? 
+                                        How can I choose the best CPQ software for my business?
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
                                     <div class="accordion-body">
-                                        <p>Select the best CPQ software by assessing your specific needs, industry fit, user-friendliness, scalability, integration capabilities, and seeking recommendations. 
+                                        <p>Select the best CPQ software by assessing your specific needs, industry fit,
+                                            user-friendliness, scalability, integration capabilities, and seeking
+                                            recommendations.
                                         </p>
-                                 </div>
+                                    </div>
                                 </template>
                             </accordion-item>
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
                                     <button class="accordion-title">
-                                       What are some of the best CPQ tools available in the market? 
+                                        What are some of the best CPQ tools available in the market?
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
                                     <div class="accordion-body">
-                                        <p>Top CPQ tools include Salesforce CPQ, Oracle CPQ Cloud, SAP CPQ, PROS Smart CPQ, and Apptivo, each with unique features and advantages. 
+                                        <p>Top CPQ tools include Salesforce CPQ, Oracle CPQ Cloud, SAP CPQ, PROS Smart CPQ,
+                                            and Apptivo, each with unique features and advantages.
                                         </p>
                                     </div>
                                 </template>
@@ -54,37 +61,41 @@
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
                                     <button class="accordion-title">
-                     How can the best CPQ software benefit the services industry?              
-                                     </button>
-                                </template>
-                                <template v-slot:accordion-content>
-                                    <div class="accordion-body">
-                                        <p>The best CPQ software helps service providers configure customized service packages, price accurately, and generate quotes efficiently, improving the sales process and service delivery. 
-                                        </p>
-                                    </div>
-                                </template>
-                            </accordion-item>
-                            <accordion-item>
-                                <template v-slot:accordion-trigger>
-                                    <button class="accordion-title">
-                                        Can CPQ software integrate with other business systems? 
+                                        How can the best CPQ software benefit the services industry?
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
                                     <div class="accordion-body">
-                                        <p>  Yes, CPQ software can integrate with CRM, ERP, and e-commerce platforms, ensuring a unified and efficient sales process throughout your organization. 
+                                        <p>The best CPQ software helps service providers configure customized service
+                                            packages, price accurately, and generate quotes efficiently, improving the sales
+                                            process and service delivery.
                                         </p>
                                     </div>
                                 </template>
                             </accordion-item>
-                       
+                            <accordion-item>
+                                <template v-slot:accordion-trigger>
+                                    <button class="accordion-title">
+                                        Can CPQ software integrate with other business systems?
+                                    </button>
+                                </template>
+                                <template v-slot:accordion-content>
+                                    <div class="accordion-body">
+                                        <p> Yes, CPQ software can integrate with CRM, ERP, and e-commerce platforms,
+                                            ensuring a unified and efficient sales process throughout your organization.
+                                        </p>
+                                    </div>
+                                </template>
+                            </accordion-item>
+
                         </accordion>
                     </div>
+                </div>
+                <div class="col-lg-2 col-md-12"></div>
             </div>
-            <div class="col-lg-2 col-md-12"></div>
         </div>
     </div>
-</div></template>
+</template>
 
 <script>
 import Accordion from "../Common/Accordion";
