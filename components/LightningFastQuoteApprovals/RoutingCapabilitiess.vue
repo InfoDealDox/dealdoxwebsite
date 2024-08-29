01923b99550a64dabd9b9d80127489ff68d1ee57
Zahira-SPM-DEALDOCX <zahira.ln@spmglobaltech.com>
2024-08-29 18:35:30 +0530

    updated quick approval code

--- components/LightningFastQuoteApprovals/RoutingCapabilitiess.vue
+++ components/LightningFastQuoteApprovals/RoutingCapabilitiess.vue
@@ -13,7 +13,7 @@
                 <div class="col-lg-6 col-md-12" >
                     <div class="about-content" style="padding-left: 30px;padding-right: 0px;">
                         <h3>Routing Capabilities</h3>
-                        <p>Whether you require single or multiple levels of authorization or need to determine the specific
+                        <p>Whether you require single or multi levels of authorization or need to determine the specific
                             order for notifying authorities, DealDox makes it easy. Our quote-to-cash platform empowers easy
                             to deal with all your statement approval concerns. With an efficient CPQ approval workflow, you
                             can
