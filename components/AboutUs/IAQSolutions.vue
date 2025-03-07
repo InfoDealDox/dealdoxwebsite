6fbf86e14f0d36246e62d6cd9485e3bf5b10986a
Zahira-SPM-DEALDOCX <zahira.ln@spmglobaltech.com>
2025-03-07 12:09:42 +0530

    updated  robots Seo requirement

--- components/AboutUs/IAQSolutions.vue
+++ components/AboutUs/IAQSolutions.vue
@@ -1,57 +1,64 @@
 <template>
     <div class="features-area pt-500 bg-white">
         <div class="container">
-            <div class="section-title-about our-team-section-title" >
+            <div class="section-title-about our-team-section-title">
                 <h1>Our Values</h1>
-                <p>At our core, we have a team of skilled developers and architects who excel in crafting state-of-the-art
+                <p>At our core, we have a team of skilled developers and architects who excel in crafting
+                    state-of-the-art
                     software solutions. With their expertise, we specialize in creating innovative and scalable software
                     that addresses the distinct requirements of every business. </p>
             </div>
             <div class="row justify-content-center" style="padding-top: 34px;">
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon1.png" alt="CPQ Systems">
+                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon1.png"
+                                alt="CPQ Systems">
                         </div>
                         <h5>Trust</h5>
-                        <p>Trust is paramount for our team. We prioritize it by fostering honest communication, maintaining
+                        <p>Trust is paramount for our team. We prioritize it by fostering honest communication,
+                            maintaining
                             transparency, and consistently fulfilling our commitments to each<br> other.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
-                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon2.png" alt="CPQ Systems">
+                            <img src="../../assets/images/cpq-integration/cpq-integration-banner-icon2.png"
+                                alt="CPQ Systems">
                         </div>
                         <h5>Collaboration</h5>
                         <p>Open communication and collaboration are the pillars of our team's success. We empower team
-                            members to come together, share ideas, solve problems collectively, and make informed decisions.
+                            members to come together, share ideas, solve problems collectively, and make informed
+                            decisions.
                         </p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
                             <img src="../../assets/images/About-US/predictiona.png" alt="CPQ Systems">
                         </div>
                         <h5>Accountability </h5>
-                        <p>By taking ownership of our work and holding ourselves and others accountable for our actions, we
+                        <p>By taking ownership of our work and holding ourselves and others accountable for our actions,
+                            we
                             ensure to actively contribute to success and <br> work towards achieving our goals.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
                             <img src="../../assets/images/cpq-integration/Automated-Oppertunity.png" alt="CPQ Systems">
                         </div>
                         <h5>Adaptability </h5>
-                        <p>We embrace change and vulnerability with an adaptable mentality, enabling us to change our course
+                        <p>We embrace change and vulnerability with an adaptable mentality, enabling us to change our
+                            course
                             and approach faster. We actively look for new opportunities and solutions in the face of
                             changing circumstances.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
                             <img src="../../assets/images/About-US/synca.png" alt="CPQ Systems">
                         </div>
@@ -62,22 +69,34 @@
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-                    <div class="single-features-item" style="height:290px">
+                    <div class="single-features-item mobile-responsiveness">
                         <div class="icon">
                             <img src="../../assets/images/About-US/income.png" alt="Complex approval processes">
                         </div>
                         <h5>Excellence </h5>
                         <p>Excellence is our relentless pursuit. We set and maintain high standards, driving each team
-                            member to excel in their role, fostering growth, and achieving exceptional outcomes across all
+                            member to excel in their role, fostering growth, and achieving exceptional outcomes across
+                            all
                             aspects of our work.</p>
                     </div>
+                </div>
             </div>
         </div>
     </div>
-</div></template>
+</template>
 
 <script>
 export default {
     name: 'IncludedBestFeatures'
 }
 </script>
+
+<style>
+@media only screen and (max-width: 550px) {
+    .mobile-responsiveness {
+        height: 206px !important;
+    }
+
+
+}
+</style>
